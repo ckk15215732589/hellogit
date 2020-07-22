1ccb89a9c55b16eb29ce9daa07bb6cee6d1d353d
ckk15215732589 <445632354@qq.com>
2020-07-22 15:24:11 +0800

    创建分支2

--- src/main/java/com/atdmkj/git/HelloGit.java
+++ src/main/java/com/atdmkj/git/HelloGit.java
@@ -2,8 +2,9 @@ package com.atdmkj.git;
 
 public class HelloGit {
     public static void main(String[] args) {
-
+        System.out.println("hello world");
         System.out.println("更新1");
         System.out.println("创建分支1");
+        System.out.println("创建分支2");
     }
 }
